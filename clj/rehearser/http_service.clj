f816cc1941169607da604c49f9d8df82419f9af9
Joonas Sarajärvi <muep@iki.fi>
2022-04-11 20:58:06 +0300

    Install session middleware
    No idea if this works at all

--- clj/rehearser/http_service.clj
+++ clj/rehearser/http_service.clj
@@ -2,6 +2,8 @@
   (:require
    [clojure.tools.logging :as log]
    [reitit.ring :as reitit-ring]
+   [ring.middleware.session :as session-middleware]
+   [ring.middleware.session.cookie :as cookie-session]
    [muuntaja.middleware :as muuntaja]
    [org.httpkit.server :as http-server]
    [hikari-cp.core :as hikari]
@@ -12,22 +14,30 @@
     (fn [req]
       (handler (assoc req :db db)))))
 
-(defn app [db] (reitit-ring/ring-handler
-                (reitit-ring/router
-                 [["/health" {:get health/get-health}]]
-                 {:data {:middleware [(wrap-db db)
-                                      muuntaja/wrap-format-response]}})
-                (reitit-ring/routes
-                 (reitit-ring/create-file-handler {:path "/"
-                                            :root "web/"})
-                 (reitit-ring/create-default-handler))))
+(defn wrap-session [key]
+  (fn [handler]
+    (session-middleware/wrap-session handler
+                                     {:store (cookie-session/cookie-store {:key key})})))
+
+(defn make-app [db key]
+  (reitit-ring/ring-handler
+   (reitit-ring/router
+    [["/health" {:get health/get-health}]]
+    {:data {:middleware [(wrap-db db)
+                         muuntaja/wrap-format-response
+                         (wrap-session key)]}})
+   (reitit-ring/routes
+    (reitit-ring/create-file-handler {:path "/"
+                                      :root "web/"})
+    (reitit-ring/create-default-handler))))
 
 (defn run [{:keys [jdbc-url port]
             :or {port 8080}}]
   (log/info "should listen on port" port "and use database at" jdbc-url)
   (let [ds (hikari/make-datasource {:jdbc-url jdbc-url})
         db {:datasource ds}
-        close-server (http-server/run-server (app db) {:port port})]
+        app (make-app db nil)
+        close-server (http-server/run-server app {:port port})]
     (fn []
       (close-server)
       (.close ds))))
