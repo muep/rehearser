e552e4d3372927911ad404a3e2338b2baee03929
Joonas Sarajärvi <muep@iki.fi>
2022-04-17 11:24:23 +0300

    Improve the support for reloading code

--- clj/user.clj
+++ clj/user.clj
@@ -1,8 +1,11 @@
 (ns user
   (:require
+   [clojure.tools.namespace.repl :refer [disable-unload! refresh]]
    [rehearser.db :refer [libpq->jdbc]]
    [rehearser.http-service :as service]
    [sysinfo :refer [sys-summary sys-stat]]))
+
+(disable-unload!)
 
 (defonce server (atom nil))
 
@@ -10,9 +13,17 @@
 (defonce port 8080)
 
 (defn run []
+  (refresh)
   (swap! server (fn [old-server]
                   (when old-server
                     (println "Shutting down" old-server)
                     (old-server))
                   (service/run {:jdbc-url (libpq->jdbc database-url)
                                 :port port}))))
+
+(defn stop []
+  (swap! server (fn [old-server]
+                  (when old-server
+                    (println "Shutting down" old-server)
+                    (old-server))
+                  nil)))
