4da42f068d6d3e5e9da74ba8e9a2fc7fa354c9a7
Joonas Sarajärvi <muep@iki.fi>
2022-07-04 20:45:04 +0300

    db check: Check for errors
    Addresses: #23 Improve error handling

--- backend/clj/rehearser/cmd/db_check.clj
+++ backend/clj/rehearser/cmd/db_check.clj
@@ -1,10 +1,18 @@
 (ns rehearser.cmd.db-check
-  (:require [clojure.java.jdbc :as jdbc]))
+  (:require [clojure.java.jdbc :as jdbc])
+  (:import (java.net ConnectException)
+           (org.postgresql.util PSQLException)))
 
 (defn run [{{:keys [jdbc-url]} :options :keys [subcmd-args] }]
   (println "Checking database at" jdbc-url)
-  (let [db {:connection-uri jdbc-url}
-        schema-ver (-> (jdbc/query db "select max(version) as version from rehearser_schema;")
-                       first
-                       :version)]
-    (println "Detecting schema version" schema-ver)))
+  (try
+    (let [db {:connection-uri jdbc-url}
+          schema-ver (-> (jdbc/query db "select max(version) as version from rehearser_schema;")
+                         first
+                         :version)]
+      (println "Detecting schema version" schema-ver))
+    (catch PSQLException e
+      (if (= ConnectException (-> e .getCause .getClass))
+        (println "Failed to connect:" (-> e .getCause .getMessage))
+        (println "PSQL error:" e))
+      (System/exit 1))))
