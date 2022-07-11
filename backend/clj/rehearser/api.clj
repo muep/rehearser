7caac32f494c8d043b0c10ca7d86841f334863b4
Joonas Sarajärvi <muep@iki.fi>
2022-07-11 22:31:00 +0300

    Add exercise get API

--- backend/clj/rehearser/api.clj
+++ backend/clj/rehearser/api.clj
@@ -1,12 +1,24 @@
 (ns rehearser.api
   (:require [rehearser.api.account :as account]
+            [rehearser.api.exercise :as exercise]
             [rehearser.exp :as exp]))
+
+(defn wrap-require-account [handler]
+  (fn [{:keys [whoami] :as req}]
+    (if (some? whoami)
+      (handler req)
+      {:status 303
+       :headers {"Location" "../login.html"}
+       :body "Logging in is required"})))
+
+(def api-metadata {:middleware [[wrap-require-account]]})
 
 (def routes [["/login" {:post account/login}]
              ["/signup" {:post account/signup}]
              ["/"]
              ["/logout" {:post account/logout}]
              ["/whoami" {:get account/whoami}]
+             ["/exercise" api-metadata exercise/routes]
              ["/params" {:get exp/params-get
                          :post exp/params-post}]
              ["/fail" {:get exp/fail
