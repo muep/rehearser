f5432d305be8d6d5575e27ae568c5621775afad1
Joonas Sarajärvi <muep@iki.fi>
2022-07-16 12:02:08 +0300

    Add api/admin/reset-db handler
    Addresses: #41 Database reset over web API

--- backend/clj/rehearser/api/admin.clj
+++ backend/clj/rehearser/api/admin.clj
@@ -1,3 +1,9 @@
-(ns rehearser.api.admin)
+(ns rehearser.api.admin
+  (:require [rehearser.db :refer [reset]]))
 
-(def routes ["/status" {:get (fn [_] {:status 200})}])
+(defn reset-db [{:keys [db]}]
+  (reset db)
+  {:status 200})
+
+(def routes [["/status" {:get (fn [_] {:status 200})}]
+             ["/reset-db" {:post reset-db}]])
