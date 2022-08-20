0e82e15b86e80ca72e56aae82108caeca575b475
Joonas Sarajärvi <muep@iki.fi>
2022-08-20 22:21:52 +0300

    Complete speccing of exercise

--- backend/clj/rehearser/api/exercise.clj
+++ backend/clj/rehearser/api/exercise.clj
@@ -3,18 +3,28 @@
             [clojure.spec.alpha :as s]))
 
 (s/def ::id int?)
+(s/def ::account-id int?)
 (s/def ::title string?)
 (s/def ::description string?)
+
+(s/def ::metadata
+  (s/keys :req-un [::id ::account-id]))
+
 (s/def ::exercise-content
   (s/keys :req-un [::title
                    ::description]))
 (s/def ::exercise
   (s/merge ::exercise-content
-           (s/keys :req-un [::id])))
+           ::metadata))
 
 (s/def ::exercise-update
-  (s/keys :opt-un [::title
-                   ::description]))
+  (s/and
+   (s/keys :opt-un [::title
+                    ::description])
+   (fn [m]
+     (-> (select-keys m [:title :description])
+         empty?
+         not))))
 
 (defn response-get-one [result]
   (if-let [body (first result)]
@@ -34,9 +44,10 @@
   {:status 200
    :body (service/find-all db whoami)})
 
-(defn post-exercise! [{:keys [db whoami body-params]
-                      :as req}]
-  {:body (service/add! db whoami body-params)
+(defn post-exercise! [{{:keys [body]} :parameters
+                       :keys [db whoami]
+                       :as req}]
+  {:body (service/add! db whoami body)
    :status 200})
 
 (defn get-exercise [{{{:keys [id]} :path} :parameters
@@ -44,24 +55,30 @@
                      :as req}]
   (response-get-one (service/find-by-id db whoami id)))
 
-(defn delete-exercise! [{{:keys [id]} :path-params
+(defn delete-exercise! [{{{:keys [id]} :path} :parameters
                          :keys [db whoami]
                          :as req}]
-  (response-modify-one (service/delete-by-id! db whoami (Integer/parseInt id))))
+  (response-modify-one (service/delete-by-id! db whoami id)))
 
 
-(defn put-exercise! [{{:keys [id]} :path-params
-                      :keys [db whoami body-params]
-                      :as req}]
-  (response-modify-one
-   (first
-    (service/update-by-id! db whoami
-                           (Integer/parseInt id)
-                           (select-keys body-params [:title :description])))))
+(defn put-exercise! [{{{:keys [id]} :path
+                       {:keys [title description]} :body} :parameters
+                      :keys [db whoami]}]
+  (let [updates (into {}
+                      (filter (fn [pair]
+                                (-> pair second some?))
+                              {:title title
+                               :description description}))]
+    (response-modify-one
+     (first
+      (service/update-by-id! db whoami id updates)))))
 
 (def routes
-  [["" {:get {:handler get-exercises}
-        :post {:handler post-exercise!}}]
+  [["" {:get {:handler get-exercises
+              :responses {200 {:body (s/* ::exercise)}}}
+        :post {:handler post-exercise!
+               :parameters {:body ::exercise-content}
+               :responses {200 {:body ::exercise}}}}]
    ["/:id" {:get {:handler get-exercise
                   :parameters {:path {:id ::id}}
                   :responses {200 {:body ::exercise}}}
@@ -69,5 +86,5 @@
                      :parameters {:path {:id ::id}}}
             :put {:handler put-exercise!
                   :parameters {:path {:id ::id}
-                               :body {:title ::title
-                                      :description ::description}}}}]])
+                               :body ::exercise-update}
+                  :responses {200 {:body ::exercise}}}}]])
