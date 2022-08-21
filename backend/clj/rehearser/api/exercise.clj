f7d0c2e165ec4dc43ad1cb2add8b64272742844c
Joonas Sarajärvi <muep@iki.fi>
2022-08-21 22:35:44 +0300

    Validate exercise API using Malli
    Addresses: #28 Validation framework

--- backend/clj/rehearser/api/exercise.clj
+++ backend/clj/rehearser/api/exercise.clj
@@ -1,5 +1,31 @@
 (ns rehearser.api.exercise
-  (:require [rehearser.service.exercise :as service]))
+  (:require [rehearser.service.exercise :as service]
+            [malli.core :as m]
+            [malli.util :as mu]
+            [malli.error :as me]))
+
+(def Metadata
+  (m/schema [:map {:closed true}
+             [:id int?]
+             [:account-id int?]]))
+
+(def ExerciseSave
+  (m/schema [:map {:closed true}
+             [:description string?]
+             [:title  (m/schema [:string {:min 1}])]]))
+
+(def Exercise
+  (mu/merge Metadata ExerciseSave))
+
+(def Exercises (m/schema [:sequential Exercise]))
+
+(def ExerciseUpdate
+  (m/schema
+   [:and
+    (mu/optional-keys ExerciseSave)
+    [:fn
+     {:error/fn (constantly "At least one update is required")}
+     (fn [m] (not (empty? m)))]]))
 
 (defn response-get-one [result]
   (if-let [body (first result)]
@@ -26,34 +52,38 @@
   {:status 200
    :body (service/find-all db whoami)})
 
-(defn post-exercise! [{:keys [db whoami body-params]
-                      :as req}]
-  {:body (service/add! db whoami body-params)
+(defn post-exercise! [{{:keys [body]} :parameters
+                       :keys [db whoami]}]
+  {:body (service/add! db whoami body)
    :status 200})
 
-(defn get-exercise [{{:keys [id]} :path-params
-                     :keys [db whoami]
-                     :as req}]
-  (response-get-one (service/find-by-id db whoami (Integer/parseInt id))))
+(defn get-exercise [{{{:keys [id]} :path} :parameters
+                     :keys [db whoami parameters]}]
+  (response-get-one (service/find-by-id db whoami id)))
 
-(defn delete-exercise! [{{:keys [id]} :path-params
-                         :keys [db whoami]
-                         :as req}]
-  (response-modify-one (service/delete-by-id! db whoami (Integer/parseInt id))))
+(defn delete-exercise! [{{{:keys [id]} :path} :parameters
+                         :keys [db whoami]}]
+  (response-modify-one (service/delete-by-id! db whoami id)))
 
-
-(defn put-exercise! [{{:keys [id]} :path-params
-                      :keys [db whoami body-params]
-                      :as req}]
+(defn put-exercise! [{{{:keys [id]} :path
+                       {:keys [title description]} :body} :parameters
+                      :keys [db whoami]}]
   (response-update-one
-   (service/update-by-id! db whoami
-                          (Integer/parseInt id)
-                          {:title (:title body-params)
-                           :description (:description body-params)})))
+   (service/update-by-id! db whoami id {:title title
+                                        :description description})))
 
 (def routes
-  [["" {:get {:handler get-exercises}
-        :post {:handler post-exercise!}}]
-   ["/:id" {:get {:handler get-exercise}
-            :delete {:handler delete-exercise!}
-            :put {:handler put-exercise!}}]])
+  [["" {:get {:handler get-exercises
+              :responses {200 {:body Exercises}}}
+        :post {:handler post-exercise!
+               :parameters {:body ExerciseSave}
+               :responses {200 {:body Exercise}}}}]
+   ["/:id" {:get {:handler get-exercise
+                  :parameters {:path {:id int?}}
+                  :responses {200 {:body Exercise}}}
+            :delete {:handler delete-exercise!
+                     :parameters {:path {:id int?}}}
+            :put {:handler put-exercise!
+                  :parameters {:path {:id int?}
+                               :body ExerciseUpdate}
+                  :responses {200 {:body Exercise}}}}]])
