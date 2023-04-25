16db3751ec46a80cb587c4a6c28c16caf3a080a4
Joonas Sarajärvi <muep@iki.fi>
2023-04-25 20:31:12 +0300

    Test also direct use of timestamp schema
    Not yet sure if the registry is a useful thing to set up

--- backend/test/rehearser/malli_test.clj
+++ backend/test/rehearser/malli_test.clj
@@ -4,17 +4,28 @@
    [malli.core :as m]
    [malli.error :as me]
    [malli.transform :as mt]
-   [rehearser.malli :refer [malli-options]])
+   [rehearser.malli :refer [Timestamp malli-options]])
   (:import
    (java.time Instant)))
 
-(t/deftest validate-instant
+(t/deftest validate-instant-via-var
+  (t/is (nil? (-> (m/explain Timestamp
+                             (Instant/now))
+                  me/humanize))))
+
+(t/deftest instant-coerce-via-var
+  (t/is (= (Instant/ofEpochSecond 1682359853)
+           (m/coerce Timestamp
+                     1682359853
+                     mt/json-transformer))))
+
+(t/deftest validate-instant-via-registry
   (t/is (nil? (-> (m/explain :timestamp
                              (Instant/now)
                              malli-options)
                   me/humanize))))
 
-(t/deftest instant-coerce
+(t/deftest instant-coerce-via-registry
   (t/is (= (Instant/ofEpochSecond 1682359853)
            (m/coerce :timestamp
                      1682359853
