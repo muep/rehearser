cca04114ef81b4ebb39476253ae161429b17ac01
Joonas Sarajärvi <muep@iki.fi>
2025-10-04 15:51:23 +0300

    Test behavior of whoami in the signup test

--- backend/test/rehearser/account_test.clj
+++ backend/test/rehearser/account_test.clj
@@ -6,11 +6,30 @@
 
     [crypto.random :as random]
     [jsonista.core :as json]
+    [ring.mock.request :as mock]
 
     [rehearser.http-service :as http-service])
   (:import (java.io ByteArrayInputStream)))
 
 (t/use-fixtures :each fixture)
+
+(def object-mapper (json/object-mapper {:decode-key-fn true}))
+
+(defn read-json-value [v]
+  (json/read-value v object-mapper))
+
+(defn handler-with-local-cookies [handler]
+  (let [cookies (atom {})]
+    (fn [req]
+      (let [req-with-cookies (reduce (fn [req [cookie-name cookie-value]]
+                                       (mock/cookie req cookie-name cookie-value))
+                                     req
+                                     @cookies)
+            response (handler req-with-cookies)]
+        (when-let [set-cookie (first (get-in response [:headers "Set-Cookie"]))]
+          (let [[cookie-name cookie-val] (clojure.string/split set-cookie #"=" 2)]
+            (swap! cookies assoc cookie-name (first (clojure.string/split cookie-val #";")))))
+        response))))
 
 (defn post-json-text-request [uri body-text]
   {:request-method :post
@@ -35,26 +54,53 @@
                               :uri            "/api/whoami"
                               :body ""})]
     (t/is (= (:status whoami-response) 200))
-    (t/is (= (-> whoami-response :body json/read-value :account-id) nil))))
+    (t/is (= (-> whoami-response :body read-json-value :account-id) nil))))
 
 (t/deftest signup-test
-  (let [app (:handler (http-service/make-app test-db (random/bytes 16) nil nil))]
+  (let [app (-> (http-service/make-app test-db (random/bytes 16) nil nil)
+                :handler
+                handler-with-local-cookies)]
+
+    ;; Initially should not get an account id from whoami
+    (let [whoami-response (app {:request-method :get
+                                :uri            "/api/whoami"
+                                :body ""})]
+      (t/is (= (:status whoami-response) 200))
+      (t/is (nil? (-> whoami-response :body read-json-value :account-id))))
+
+    ;; Let's make an account from scratch
     (let [response (app (post-form-request
-                          "/api/signup"
-                          {:username "bobb" :password "s3kret"}))]
+                         "/api/signup"
+                         {:username "bobb" :password "s3kret"}))]
       (t/is (= (:status response) 303))
       (t/is (= (-> response :headers (get "Location")) "../login.html")))
 
+    ;; Attempt logging in with a bad password
     (let [response (app (post-form-request
-                          "/api/login"
-                          {:username "bobb" :password "badpw"}))]
+                         "/api/login"
+                         {:username "bobb" :password "badpw"}))]
       (t/is (= (:status response) 303))
       ;; This is pretty bad API, basically no indication of success
       ;; or failure except for the redirect URI
       (t/is (= (-> response :headers (get "Location")) "../login.html")))
 
+    ;; Still should not get an account id from whoami
+    (let [whoami-response (app {:request-method :get
+                                :uri            "/api/whoami"
+                                :body ""})]
+      (t/is (= (:status whoami-response) 200))
+      (t/is (nil? (-> whoami-response :body read-json-value :account-id))))
+
+    ;; Log in with the correct password
     (let [response (app (post-form-request
-                          "/api/login"
-                          {:username "bobb" :password "s3kret"}))]
+                         "/api/login"
+                         {:username "bobb" :password "s3kret"}))]
       (t/is (= (:status response) 303))
-      (t/is (= (-> response :headers (get "Location")) "../index.html")))))
+      (t/is (= (-> response :headers (get "Location")) "../index.html")))
+
+    ;; Now the API should report an user id
+    (let [whoami-response (app {:request-method :get
+                                :uri            "/api/whoami"
+                                :body ""})]
+      (t/is (= (:status whoami-response) 200))
+      (t/is (integer? (-> whoami-response :body read-json-value :account-id))))))
