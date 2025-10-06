07f538d9c623fc59a5a42e9fec21328a3887f2a0
Joonas Sarajärvi <muep@iki.fi>
2025-10-06 20:59:55 +0300

    Fix the test filtering tooling

--- backend/test/rehearser/testsuite.clj
+++ backend/test/rehearser/testsuite.clj
@@ -6,23 +6,29 @@
 (defn- test-pattern [kw]
   (re-pattern (str "rehearser\\..*" kw ".*-test")))
 
+(defn- matches-some-pattern [v patterns]
+  (let [m (meta v)
+        name (str (ns-name (:ns m)) "/" (:name m))]
+    (some #(re-find % name) patterns)))
+
 (defn -main [& args]
   (test-db/wrap-prepared-template-db!
     (fn []
-      (let [pattern (if (seq args)
-                      (map test-pattern args)
+      (let [patterns (if (seq args)
+                      (mapv test-pattern args)
                       [#"rehearser\..*-test"])
             {:keys [reporter timings]} (test-time-reporter
                                         (if (nil? (System/console))
                                           eftest.report.pretty/report
                                           eftest.report.progress/report))]
-        (eftest/run-tests
-         (eftest/find-tests "backend/test")
-         {:include pattern
-          :fail-fast? true
-          :capture-output? true
-          :multithread? true
-          :report reporter})
+        (as-> (eftest/find-tests "backend/test") $
+          (filterv #(matches-some-pattern % patterns) $)
+          (eftest/run-tests
+           $
+           {:fail-fast? true
+            :capture-output? true
+            :multithread? true
+            :report reporter}))
 
         (println "\nTest durations:")
         (doseq [[v ms] (sort-by second @timings)]
