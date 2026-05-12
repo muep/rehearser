01c49f64049b9ff3c3d2de7ad4e26e01e9be44e5
Joonas Sarajärvi <muep@iki.fi>
2026-05-12 21:34:35 +0300

    Suggestions in the rehearsal page

--- backend/clj/rehearser/ui/rehearsals/detail.clj
+++ backend/clj/rehearser/ui/rehearsals/detail.clj
@@ -1,19 +1,22 @@
 (ns rehearser.ui.rehearsals.detail
   (:require
    [hiccup.core :as hiccup]
+   [rehearser.service.plan :as plan]
    [rehearser.service.rehearsal :as rehearsal-service]
    [rehearser.ui.common :as common-ui]
    [rehearser.ui.rehearsals.components :as components])
   (:import (java.time Duration Instant)))
 
 (defn rehearsal-page [{{{:keys [id]} :path} :parameters
-                       :keys [db url-prefix whoami]}]
+                       :keys [db url-prefix whoami mistral-api-key]}]
   (if-let [rehearsal (rehearsal-service/find-rehearsal db whoami id)]
     (let [{:keys [start-time duration title description entries]} rehearsal
           now (Instant/now)
           seconds (if duration
                     duration
-                    (.getSeconds (Duration/between start-time now)))]
+                    (.getSeconds (Duration/between start-time now)))
+          all-rehearsals (rehearsal-service/find-all db whoami)
+          suggestions-data (plan/suggestions whoami)]
       {:status 200
        :body
        (common-ui/page
@@ -45,6 +48,36 @@
             [:li (components/entry-link entry url-prefix)
              " at " (hiccup/h (components/format-time entry-time))])]
 
+         (when mistral-api-key
+           [:form {:method "post" :action (str url-prefix "/rehearsals/" id "/initiate-suggestions.html")}
+            [:button {:type "submit"} "Refresh Suggestions"]])
+
+         (when suggestions-data
+           (if-let [error (:error suggestions-data)]
+             [:div {:class "error"} "Error: " (hiccup/h error)]
+             (let [{:keys [suggestions current-rehearsal reference-rehearsal-ids]} suggestions-data]
+               [:div
+                (when (and current-rehearsal (= id (:id current-rehearsal)))
+                  [:div
+                   [:h2 "Reference Rehearsals"]
+                   [:ul
+                    (for [ref-id reference-rehearsal-ids
+                          :let [ref-rehearsal (first (filter #(= ref-id (:id %)) all-rehearsals))]]
+                      [:li (components/rehearsal-link ref-rehearsal url-prefix)])]])
+
+                (when (seq suggestions)
+                  [:div
+                   [:h2 "Suggestions"]
+                   [:ul
+                    (for [{:keys [exercise-id title rationale]} suggestions]
+                      [:li
+                       [:strong (hiccup/h title)]
+                       " - " (hiccup/h rationale)
+                       [:form {:action (str url-prefix "/rehearsals/" id "/new-entry.html")
+                               :method "get" :style "display: inline"}
+                        [:input {:type "hidden" :name "exercise-id" :value exercise-id}]
+                        [:button {:type "submit"} "Add"]]])]])])))
+
          [:h2 "Other"]
          [:a {:href (str url-prefix "/rehearsals/" id "/edit.html")} "Edit"]])})
     {:status 404
@@ -62,6 +95,13 @@
   {:status 303
    :headers {"location" (str url-prefix "/rehearsals/" id "/rehearsal.html")}})
 
+(defn rehearsal-initiate-suggestions! [{{{:keys [id]} :path} :parameters
+                                       :keys [db url-prefix whoami mistral-api-key]}]
+  (when mistral-api-key
+    (plan/initiate-suggestions db whoami mistral-api-key))
+  {:status 303
+   :headers {"location" (str url-prefix "/rehearsals/" id "/rehearsal.html")}})
+
 (def routes
   [["/rehearsals/:id/rehearsal.html"
     {:get {:parameters {:path {:id int?}}}
@@ -71,6 +111,7 @@
             :handler rehearsal-close!}}]
    ["/rehearsals/:id/open.html"
     {:post {:parameters {:path {:id int?}}
-            :handler rehearsal-open!}}]])
-
-
+            :handler rehearsal-open!}}]
+   ["/rehearsals/:id/initiate-suggestions.html"
+    {:post {:parameters {:path {:id int?}}
+            :handler rehearsal-initiate-suggestions!}}]])
